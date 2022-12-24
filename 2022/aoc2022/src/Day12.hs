9c7bc9e7455ac53b995ba625a046c6597b982a2f
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-12-24 13:23:32 +0900

    I was able to get the correct answer for the example input, but the real input is either infinite looping or taking way too long
    I think I need to profile the short case and see if there's a bottleneck
    
    it's probably because it's not tail recursive, but I don't know how I
    could compare the length of the possible paths if I did it tail
    recursively

--- 2022/aoc2022/src/Day12.hs
+++ 2022/aoc2022/src/Day12.hs
@@ -31,16 +31,48 @@ run inputLines = do
     let asOneInput = concat inputLines
     --print asOneInput
     let startIndex = L.elemIndex 'S' asOneInput
-    let charsWithIndices = indexedList asOneInput
-    --print charsWithIndices
-    let charsWithIndicesAndValues = map (\(x,idx) -> (x, ord x, idx)) charsWithIndices
-    --print charsWithIndicesAndValues
-    let asVector = V.fromList charsWithIndicesAndValues
-    --print asVector
-    let nodesWithNeighbors = makeGraph lengthOfRow asVector
-    -- TODO some nodes are having their height values swapped with
-    -- the index somewhere...
-    print nodesWithNeighbors
+    case startIndex of
+        Nothing -> print "couldn't find start index!!!"
+        Just sIdx -> do
+            let charsWithIndices = indexedList asOneInput
+            --print charsWithIndices
+            let charsWithIndicesAndValues = map (\(x,idx) -> (x, evaluateCharacter x, idx)) charsWithIndices
+            --print charsWithIndicesAndValues
+            let asVector = V.fromList charsWithIndicesAndValues
+            --print asVector
+            let nodesWithNeighbors = makeGraph lengthOfRow asVector
+            -- TODO some nodes are having their height values swapped with
+            -- the index somewhere...
+            print nodesWithNeighbors
+            let path = findEnd nodesWithNeighbors 'E' [] sIdx
+            case path of
+                Just p -> print $ "found path:" ++ show p ++ " of length:" ++ show (length p - 1)
+                Nothing -> print "couldn't find a path!!!!"
+            --print path
+
+-- I had to re-read the problem:
+--    "Your current position (S) has elevation a, and the location that
+--    should get the best signal (E) has elevation z."
+evaluateCharacter :: Char -> Int
+evaluateCharacter c = case c of
+    'E' -> ord 'z'
+    'S' -> ord 'a'
+    normLetter -> ord normLetter
+
+findEnd :: V.Vector Node -> Char -> [Index] -> Index -> Maybe [Index]
+findEnd graph endChar visitedList startIdx = case thisNode of
+    Nothing -> trace ("bad node idx:" ++ show startIdx) Nothing
+    Just (Node c _ idx neighbors) ->
+        if startIdx `elem` visitedList then Nothing
+        else
+            if c == endChar
+            then Just [idx]
+            else let possibleSolutions = map (findEnd graph endChar (idx:visitedList)) neighbors in
+                let solutions = L.sortOn length (catMaybes possibleSolutions) in
+                    if null solutions then Nothing else Just $ idx:head solutions
+                --Just $ idx:head 
+    where
+        thisNode = getNodeFromGraph graph startIdx
 
 makeGraph :: Int -> V.Vector LabeledChar -> V.Vector Node
 makeGraph numItemsPerRow labeledNodes = findNeighbors numItemsPerRow 0 labeledNodes V.empty
@@ -61,12 +93,6 @@ findNeighbors itemsPerRow currIdx graphIn graphOut =
             left = getNodeFromGraphIn (currIdx - 1)
             right = getNodeFromGraphIn (currIdx + 1)
             getNodeFromGraphIn = getNodeFromGraph graphIn
-    --let taggedChars = indexed asOneInput
-    --when (isNothing startIndex) (die "no start element")
-    --let (path, _) = findPath lengthOfRow [] asOneInput [fromJust startIndex] (fromJust startIndex)
-    --case path of
-    --    Nothing -> print "no path found?!?"
-    --    Just p -> print $ "path:" ++ p ++ " path length:" ++ show (length p)
 
 getNodeFromGraph :: V.Vector a -> Int -> Maybe a
 getNodeFromGraph graph idx = if checkIndex (V.length graph) idx
@@ -74,15 +100,19 @@ getNodeFromGraph graph idx = if checkIndex (V.length graph) idx
     else Nothing
 
 -- returns true if it is possible to climb from location a to location b
--- the special cases are 'S', which you can never climb to (you start there)
--- and 'E', the end
+-- the special cases are 'S', which you can never climb to (you start there), and can always climb from
+-- and 'E', the end, which you can always climb to
 -- you can climb up at most one elevation
 -- you can always climb down
+-- I had to re-read the problem:
+--    "Your current position (S) has elevation a, and the location that
+--    should get the best signal (E) has elevation z."
 canClimbTo :: (Char, Int) -> (Char, Int) -> Bool
-canClimbTo _ ('E', _) = True
-canClimbTo ('S', _) _ = True
-canClimbTo _ ('S', _) = False
-canClimbTo (_, fVal) (_, tVal) = trace ("climb from height:" ++ show fVal ++ " to height:" ++ show tVal) (tVal <= (fVal + 1))
+--canClimbTo _ ('E', _) = True
+--canClimbTo ('S', _) _ = True
+--canClimbTo _ ('S', _) = False
+--canClimbTo (_, fVal) (_, tVal) = trace ("climb from height:" ++ show fVal ++ " to height:" ++ show tVal) (tVal <= (fVal + 1))
+canClimbTo (_, fVal) (_, tVal) = tVal <= fVal + 1
 
 checkIndex :: Int -> Int -> Bool
 checkIndex numItems targetIndex = targetIndex >= 0 && targetIndex < numItems

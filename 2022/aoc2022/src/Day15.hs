fe451a5198eb52ef81ed99a3c90d886f1ab825e9
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2023-03-23 23:15:50 +0900

    I am working on a way to test to confirm that the area around a sensor is marked as it being impossible for a beacon to be there

--- 2022/aoc2022/src/Day15.hs
+++ 2022/aoc2022/src/Day15.hs
@@ -4,11 +4,23 @@ import qualified Text.Parsec as P
 
 import Data.Functor
 
-import qualified Data.Set as S
+import Debug.Trace
 
 import Data.List (
     unfoldr
     , sort
+    )
+
+import qualified Data.Set as S (
+    fromList
+    , difference
+    , union
+    , unions
+    , Set
+    , toList
+    , fromList
+    , intersection
+    , empty
     )
 
 import System.IO.Strict as ST (
@@ -32,11 +44,54 @@ run inputFilePath = do
     case P.runParser parse () inputFilePath fileContents of
         Left parseError -> print parseError
         Right parseResults -> do
-            let inRangeOfSensor = calculateImpossiblePlacesForY checkYCoord parseResults
-            let cannotBeBeacon = postProcessImpossiblePlaces inRangeOfSensor parseResults
-            print $ "checking y coord:" ++ show checkYCoord
-            --print $ sort cannotBeBeacon
-            print $ "num impossible places:" ++ show (length cannotBeBeacon)
+            --let inRangeOfSensor = calculateImpossiblePlacesForY checkYCoord parseResults
+            --let cannotBeBeacon = postProcessImpossiblePlaces inRangeOfSensor parseResults
+            --print $ "checking y coord:" ++ show checkYCoord
+            ----print $ sort cannotBeBeacon
+            --print $ "num impossible places:" ++ show (length cannotBeBeacon)
+            print "part2"
+            --let gridDim = targetBeaconMaxCoord
+            let gridDim = 20
+            print $ "maxCoord:" ++ show gridDim
+            let prohibitedSpaces = S.unions $ map getProhibitedSpacesAroundSensor parseResults
+            let grid = getGrid gridDim gridDim
+            --print $ "grid:" ++ show grid
+            print $ "prohibitedSpaces:" ++ show prohibitedSpaces
+            --print $ "difference:" ++ show (S.difference grid prohibitedSpaces)
+
+data Test = Test String [Coord]
+
+test :: IO ()
+test = do
+    let tests = [
+            Test "Sensor at x=14, y=3: closest beacon is at x=15, y=3"
+                [
+                    (14, 3), (14, 2), (14, 4)
+                    , (13, 3), (13, 4), (13, 2)
+                    , (15, 3), (15, 2), (15, 4)
+                    ]
+            ]
+    testResults <- mapM runTest tests
+    mapM_ print testResults
+
+runTest :: Test -> IO (String, Bool, S.Set Coord)
+runTest (Test input answers) = do
+    case P.runParser parseLine () "" input of
+        Left e -> print ("couldn't run test:" ++ input ++ " " ++ show e) >> return (input, False, S.empty)
+        Right sensor -> return (input, null diff, diff)
+            where
+                diff = S.difference (S.fromList answers) results
+                results = getProhibitedSpacesAroundSensor sensor
+
+
+getGrid :: Int -> Int -> S.Set Coord
+getGrid x y = S.fromList (concat [ ([(cx, cy) | cx <- [0..x]]) | cy <- [0..y] ])
+
+targetBeaconMaxCoord :: Int
+targetBeaconMaxCoord = 4 * ((10 :: Int) ^ (6 :: Int))
+
+tuningFrequency :: Int -> Int -> Int
+tuningFrequency x y = x * targetBeaconMaxCoord + y
 
 -- if the coordinate happened to be a beacon, it needs removed from
 -- the list of spaces that couldn't possibly be a beacon
@@ -67,6 +122,31 @@ calculateImpossiblePlacesForY y sensors = go [] (filter (isCloseTo y) sensors)
     go prohibited [] = prohibited
     go prohibited (s:ss) = go (prohibited++getProhibitedSpacesAtY y s) ss
 
+getProhibitedSpacesAroundSensor :: Sensor -> S.Set Coord
+getProhibitedSpacesAroundSensor (Sensor (sx, sy) _ dist) =
+    let upProhibited = walkItBack (trace (show furthestUp) furthestUp) countDown stopUp
+        downProhibited = walkItBack (trace (show furthestDown) furthestDown) countUp stopDown
+    in
+        S.union (S.fromList upProhibited) (S.fromList downProhibited)
+    where
+        furthestUp = (sx, sy - dist)
+        furthestDown = (sx, sy + dist)
+        stopDown (_, y) = y == sy - 1
+        countDown (x, y) = (x + 1, y + 1)
+        stopUp (_, y) = y == sy
+        countUp (x, y) = (x + 1, y - 1)
+
+walkItBack :: Coord -> (Coord -> Coord) -> (Coord -> Bool) -> [Coord]
+walkItBack start update stop  = go [start] 1 start
+    where
+        go :: [Coord] -> Int -> Coord -> [Coord]
+        go acc iter currCoord@(cx, cy) =
+            if stop currCoord
+            then acc
+            else go (acc ++ newProhibited) (iter + 2) (update currCoord)
+            where
+                --newProhibited = [ (x, cy) | x <- [1..iter] ]
+                newProhibited = foldl (\newPAcc newX -> (cx - newX, cy):newPAcc) [] [1..iter] -- [ (x, cy) | x <- [1..iter] ]
 
 -- this assumes that the Y coordinate is in range of the sensor
 getProhibitedSpacesAtY :: Int -> Sensor -> [Coord]

e841eabd7643f90d686e42a1f2711cdb704fce99
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2023-07-22 09:24:09 +0900

    tried to make the bash script more portable by not harding the path to be directly under the home directory

---
+++ 2022/run.sh
@@ -0,0 +1,15 @@
+#FILENAME="day9-short.txt"
+#FILENAME="day9-short-2.txt"
+FILENAME="day19-short.txt"
+
+#https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+#echo "scriptDir:$SCRIPT_DIR"
+INPUTS_PATH="../inputs/$FILENAME"
+
+pushd "$SCRIPT_DIR/aoc2022"
+stack run -- "$INPUTS_PATH"
+popd
+
+#stack exec --profile --  aoc2022-exe $SCRIPT_DIR/advent-of-code/2022/2022/inputs/$FILENAME
+#stack run --profile -- $SCRIPT_DIR/advent-of-code/2022/2022/inputs/$FILENAME +RTS -p

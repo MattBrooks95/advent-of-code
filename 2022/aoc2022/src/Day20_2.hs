c5c12d16c5fbb2571d488305cf3f4d7d9f9e004e
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2023-11-05 11:37:25 +0900

    newtypes, turned into a Data.Sequence

--- 2022/aoc2022/src/Day20_2.hs
+++ 2022/aoc2022/src/Day20_2.hs
@@ -8,23 +8,38 @@ import System.Exit (die)
 import qualified Data.ByteString as BSS (
     readFile
     )
-import qualified Data.Word8 as W 
+
 import qualified Data.ByteString.Lazy as BSL
-import qualified Data.ByteString as BS
 import qualified Data.Attoparsec.ByteString.Char8 as APC
 import qualified Data.Attoparsec.ByteString.Lazy as AP
-import Data.Either (isLeft)
-import Control.Monad (
-    when
-    )
+import qualified Data.Sequence as S
+
+newtype MixItemValue = MixItemValue Int
+instance Show MixItemValue where
+    show (MixItemValue v) = "v:" <> show v
+newtype MixItemIndex = MixItemIndex Int
+instance Show MixItemIndex where
+    show (MixItemIndex v) = "i:" <> show v
+newtype MixItem = MixItem (MixItemValue, MixItemIndex)
+instance Show MixItem where
+    show (MixItem (v, i)) = "(" <> show v <> ", " <> show i <> ")"
 
 run :: String -> IO ()
 run inputFilePath = do
-    fileContents <- BSS.readFile inputFilePath
-    let parsedResult = AP.parseOnly parseInput (BSL.fromStrict fileContents)
-    when (isLeft parsedResult) (die "failed to parse")
     print $ "day20 attempt 2:" <> " using file at path:" <> inputFilePath
-    print parsedResult
+    fileContents <- BSS.readFile inputFilePath
+    parsedResult <-
+        case AP.parseOnly parseInput (BSL.fromStrict fileContents) of
+            Left err -> die ("failed to parse" <> err)
+            Right pr -> pure pr
+    --print parsedResult
+    let values = map MixItemValue parsedResult
+        indices = map MixItemIndex [0..]
+        --withIndices = map MixItem (zip values indices)
+        --use zipWith
+        withIndices = zipWith (curry MixItem) values indices
+        itemsSequence = S.fromList withIndices
+    print itemsSequence
 
 parseInput :: AP.Parser [Int]
 parseInput = AP.sepBy parseInputNumber APC.endOfLine

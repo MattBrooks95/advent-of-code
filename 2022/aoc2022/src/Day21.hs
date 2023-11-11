eb1ac7fa9ee7f573aa8740e7161bcee4c1e6f065
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2023-11-11 09:08:52 +0900

    let's sketch out some types

--- 2022/aoc2022/src/Day21.hs
+++ 2022/aoc2022/src/Day21.hs
@@ -16,5 +16,18 @@ run fp = do
     fileContents <- BSS.readFile fp
     print "Day21"
 
-parse :: AP.Parser
+newtype MonkeyName = MonkeyName String
+
+data Op = Add | Sub | Mult | Div
+
+-- | it looks like Monkeys that have expressions can't do math with literals, on the result of evaluating other monkeys
+data Expression = Expression {
+    expArg1 :: MonkeyName
+    , expArg2 :: MonkeyName
+    , expOp :: Op
+    }
+
+data Monkey = Evaluated Double | LiteralMonkey Int | ExpressionMonkey Expression
+
+parse :: AP.Parser [Monkey]
 parse = undefined

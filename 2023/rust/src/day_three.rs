da03eaa54b7ab322578e281c602d136cc0504129
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2023-12-05 20:33:42 +0900

    I guess I can't have a list in here because their size can't be known at compile time.

--- 2023/rust/src/day_three.rs
+++ 2023/rust/src/day_three.rs
@@ -1,18 +1,28 @@
 use nom::IResult;
 
-//the top left corner of the input is 0,0
 //X values increase from 0 to W, left to right
 struct X(i32);
 //Y values increase from 0 to H, top to bottom
 struct Y(i32);
 
+//the top left corner of the input is 0,0
 type Location = (X, Y);
 
 //a special character is the character and a location
 struct SpecialSymbol(char, Location);
 
-//a number is a numeric value and a list of the locations of each of the digits
-struct Number(i32, [Location]);
+struct Value(i32);
+struct NumDigits(i32);
+//digits are a numeric value and the number of digits in the value
+//the # of digits can be used to calculate the location span of the number's digits
+struct Digits(Value, NumDigits);
+
+enum BoardEntity {
+    Symbol(SpecialSymbol),
+    Number(Digits),
+}
+
+type Board = Vec<BoardEntity>;
 
 pub fn run(file_paths: [&str; 2]) {
     println!("day three, files:{:?}", file_paths);
@@ -26,5 +36,5 @@ fn do_file(file_path: &str) -> () {
     let parse_result = nom::multi::many1(parse_line)(&contents);
 }
 
-fn parse_line(input: &str) -> IResult<&str, _> {
+fn parse_line(input: &str) -> IResult<&str, Board> {
 }

483bcf36f712325fbadcad47dc1a1d5d81cf3da0
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2023-12-06 13:59:42 +0900

    gotta be sure to find the start of the digits

--- 2023/rust/src/day_three.rs
+++ 2023/rust/src/day_three.rs
@@ -1,8 +1,8 @@
 //X values increase from 0 to W, left to right
-#[derive(Eq, PartialEq, Hash, Clone, Debug)]
+#[derive(Eq, PartialEq, Hash, Clone, Copy, Debug)]
 struct X(usize);
 //Y values increase from 0 to H, top to bottom
-#[derive(Eq, PartialEq, Hash, Clone, Debug)]
+#[derive(Eq, PartialEq, Hash, Clone, Copy, Debug)]
 struct Y(usize);
 
 //the top left corner of the input is 0,0
@@ -39,6 +39,7 @@ pub fn run(file_paths: [&str; 2]) {
 struct Input(char, X, Y);
 
 type SpecialCharsMap = std::collections::HashMap<Location, SpecialSymbol>;
+type DigitHitSet = std::collections::HashSet<Location>;
 
 fn do_file(file_path: &str) -> () {
     println!("file:{}", file_path);
@@ -63,6 +64,60 @@ fn do_file(file_path: &str) -> () {
     let special_char_map: SpecialCharsMap = get_special_character_map(&character_matrix);
     //println!("{:?}", character_matrix);
     println!("{:?}", special_char_map);
+    let touch_symbol_digits = get_digits_from_symbols(&character_matrix, &special_char_map);
+}
+
+fn get_digits_from_symbols(
+    char_matrix: &Vec<Vec<Input>>,
+    special_char_map: &SpecialCharsMap
+) -> DigitHitSet {
+    let mut set = std::collections::HashSet::new();
+    let touching_digits_start_location = special_char_map
+        .into_iter()
+        .for_each(|(loc@Location(x, y), _)| {
+            let search_locations = get_check_locations(loc);
+            let digits: Vec<_> = search_locations
+                .into_iter()
+                //TODO be careful about the access order here
+                //I think when I made the character matrix, y (rows) is the outer array
+                //index, and x (cols) is the inner array index
+                .map(|Location(X(x), Y(y))| {
+                    let Input(c, _, _) = char_matrix.get(y)?.get(x);
+                    if (c.is_digit()) {
+                    } else {
+                        None
+                    }
+                })
+                .filter(Option::is_some)
+                .map(Option::unwrap)
+                .collect();
+        });
+    set
+}
+
+fn find_first_digit(Location(X(x), Y(y)): &Location, char_matrix: &Vec<Vec<Input>>) -> Location {
+    
+}
+
+fn get_check_locations(Location(this_x@X(x), this_y@Y(y)): &Location) -> Vec<Location> {
+    let x_prev = X(x - 1);
+    let x_next = X(x + 1);
+    let this_x_clone = this_x.clone();
+    let this_y_clone = this_y.clone();
+    //TODO does Rust have list comprehensions?
+    vec!(
+        Location(x_prev, Y(y - 1)),
+        Location(x_prev, this_y_clone),
+        Location(x_prev, Y(y + 1)),
+
+        Location(this_x_clone, Y(y - 1)),
+        Location(this_x_clone, this_y_clone),
+        Location(this_x_clone, Y(y + 1)),
+
+        Location(x_next, Y(y - 1)),
+        Location(x_next, this_y_clone),
+        Location(x_next, Y(y+1))
+    )
 }
 
 fn get_special_character_map(char_matrix: &Vec<Vec<Input>>) -> SpecialCharsMap {
